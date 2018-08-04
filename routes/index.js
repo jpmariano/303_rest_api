b9fbc8e4fab65f30ba2abe537275af5f33345b46
John Paul Mariano <jppm8387@yahoo.com>
2018-08-04 16:49:53 -0400

    Clean up

--- routes/index.js
+++ routes/index.js
@@ -39,6 +39,7 @@ router.get('/books', (req, res) => {
 
 /* GET speakers detail page */
 router.get('/books/:bookid', function(req, res) {
+  
   res.render('book', {
     title: appdata.books[req.params.bookid].title,
     author: appdata.books[req.params.bookid].author,
@@ -47,91 +48,26 @@ router.get('/books/:bookid', function(req, res) {
   
 });
 
-router.get('/allspeakers', (req, res) => {
-  speaker
-    .find()
-    .then(speakers => {
-      res.json(speakers.map(speaker => speaker.apiRepr()));
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    }); 
-});
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  var myArtwork = [];
-  this.myArtists = [];
-
-
-  speaker
-    .find()
-    .then(speakers => {
-       set_myArtists(speakers.map(speaker => speaker.apiRepr()));
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    }); 
-    
-
-  function set_myArtists(artist){
-    this.myArtists = artist;
-    //console.log(artist);
-  }  
-  
-//myArtists = appdata.speakers;
-
-  myArtists.forEach(function(item) {
-    myArtwork = myArtwork.concat(item.artwork);
-  });
- 
-  res.render('index', {
-    title: 'Home',
-    artwork: myArtwork,
-    artists: myArtists,
-    page: 'home'
-  });
-});
-
-/* GET speakers page. */
-router.get('/speakers', function(req, res) {
-  var myArtwork = [];
-  var myArtists = [];
-  myArtists = appdata.speakers;
-
-  appdata.speakers.forEach(function(item) {
-    myArtwork = myArtwork.concat(item.artwork);
-  });
-  res.render('speakers', {
-    title: 'Speakers',
-    artwork: myArtwork,
-    artists: myArtists,
-    page: 'artistList'
-  });
-});
-
-
-/* GET speakers detail page */
-router.get('/speakers/:speakerid', function(req, res) {
-  var myArtwork = [];
-  var myArtists = [];
-
-  appdata.speakers.forEach(function(item) {
-    if (item.shortname == req.params.speakerid) {
-      myArtists.push(item);
-      myArtwork = myArtwork.concat(item.artwork);
+  book
+  .find()
+  .then(books => {
+    if(books.length < 1 || books == undefined ){
+      books = appdata.books;
+      res.json(books);
+    } else {
+      res.json(books.map(book => book.apiRepr()));
     }
-  });
-  res.render('speakers', {
-    title: 'Speakers',
-    artwork: myArtwork,
-    artists: myArtists,
-    page: 'artistDetail'
+    
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error: 'something went terribly wrong'});
   });
 });
-
 
 
 
